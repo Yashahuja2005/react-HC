ddddf7eb51c9fdf2613eba62a07839585d843c0e
Yashahuja2005 <ahujayash756@gmail.com>
2025-11-08 13:20:27 +0530

    Custom React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Maggi from "./Maggi.jsx"
+function App() {
+  const username = "Yash Ahuja"
+  return (
+    <>
+      <h1>React with vite | {username}</h1>   //evaluatted expression
+      <Maggi />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Maggi.jsx
@@ -0,0 +1,5 @@
+function Maggi(){
+    return(
+        <h3>Maggi is ready</h3>
+    )
+}export default Maggi
