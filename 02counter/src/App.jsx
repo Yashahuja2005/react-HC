8ed290b76c4801b409166112b15fb91d6d20ce9b
Yashahuja2005 <ahujayash756@gmail.com>
2025-11-08 16:51:27 +0530

    Use of useStates in React

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  // let counter = 5
+
+  const addValue = () => {
+    
+    
+  if (counter < 20) {
+    setCounter(counter + 1);
+    console.log('clicked', Math.random());
+  }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+  return (
+    <>
+      <h1>Maggi aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
